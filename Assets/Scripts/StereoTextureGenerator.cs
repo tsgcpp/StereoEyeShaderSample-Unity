0db54a1e39ce87c11607037ab9421573398ac31a
tsgcpp <tsgcpp.public8x@gmail.com>
2020-06-23 13:44:26 +0900

    Add a sample of StereoEyeShader.

---
+++ Assets/Scripts/StereoTextureGenerator.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+[RequireComponent(typeof(Renderer))]
+public class StereoTextureGenerator : MonoBehaviour
+{
+    public Texture leftEyeTexture;
+    public Texture rightEyeTexture;
+
+    private Renderer _renderer;
+
+    private RenderTexture _stereoTexture;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+        _stereoTexture = GenerateStereoTexture(
+            leftEyeTexture, rightEyeTexture);
+
+        // _MainTexはTex2DArrayとして受け取ることを前提
+        _renderer.sharedMaterial.mainTexture = _stereoTexture;
+    }
+
+    /// <summary>
+    /// Tex2DArray型のRenderTextureを生成し、
+    /// 両目用テクスチャをそれぞれのテクスチャにコピーして返す
+    /// </summary>
+    /// <param name="leftEyeTexture">左目用テクスチャ</param>
+    /// <param name="rightEyeTexture">右目用テクスチャ</param>
+    /// <param name="targetRT">コピー先のRenderTexture(Tex2DArrayかつvolumeであることが前提)</param>
+    private static RenderTexture GenerateStereoTexture(
+        Texture leftEyeTexture, Texture rightEyeTexture)
+    {
+        var desc = new RenderTextureDescriptor(1024, 1024);
+
+        // RenderTextureをTex2DArrayとして生成
+        desc.dimension = TextureDimension.Tex2DArray;
+
+        // Textureは2つ(0番目は左目用、1番目は右目用)
+        desc.volumeDepth = 2;
+        
+        // RenderTextureDescriptorからテクスチャを生成
+        var stereoTexture = RenderTexture.GetTemporary(desc);
+
+        // 両目用テクスチャをTex2DArray型のRenderTextureにコピー
+        Graphics.Blit(
+            source: leftEyeTexture, dest: stereoTexture,
+            sourceDepthSlice: 0, destDepthSlice: 0);
+        Graphics.Blit(
+            source: rightEyeTexture, dest: stereoTexture,
+            sourceDepthSlice: 0, destDepthSlice: 1);
+
+        return stereoTexture;
+    }
+
+    private void OnDestroy()
+    {
+        _renderer.sharedMaterial.mainTexture = null;
+        RenderTexture.ReleaseTemporary(_stereoTexture);
+        _stereoTexture = null;
+    }
+}
---
+++ Assets/Shaders/StereoEyeTexture.shader
@@ -0,0 +1,88 @@
+﻿Shader "StereoEyeTexture"
+{
+    SubShader
+    {
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+			// texture arrayが有効なプラットフォームのみで子のシェーダーが使用可能であることの宣言
+            #pragma require 2darray
+            
+            #include "UnityCG.cginc"
+
+			/*
+			unity_StereoEyeIndexについて
+
+			unity_StereoEyeIndexはXR環境であれば基本的に`#include`経由で変数宣言される。
+			実行中のシェーディング処理が左目もしくは右目のどっち向けかの判定に使用される。
+			値が 0: 左目、1: 右目。
+			*/
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+
+				// instanceID変数の宣言
+				UNITY_VERTEX_INPUT_INSTANCE_ID
+			};
+
+			struct v2f
+			{
+
+				float4 vertex : SV_POSITION;
+				float2 uv : TEXCOORD0;
+
+				// instanceIDをfragに送る場合は必要(今回は不要)
+				// UNITY_VERTEX_INPUT_INSTANCE_ID
+
+				// fragにunity_StereoEyeIndexなどの情報を流すための変数を追加
+				UNITY_VERTEX_OUTPUT_STEREO
+			};
+
+			v2f vert (appdata v)
+			{
+				v2f output;
+
+				// instanceIDおよびunity_StereoEyeIndexの取得
+				UNITY_SETUP_INSTANCE_ID(v);
+
+				// instanceIDをfragに送る場合は必要(今回は不要)
+				// UNITY_TRANSFER_INSTANCE_ID(v, o);
+
+				// outputの初期化
+				UNITY_INITIALIZE_OUTPUT(v2f, output);
+
+				// unity_StereoEyeIndexをoutput内の変数に付与
+				UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(output);
+				
+				// vertexとuvは通常通り
+				output.vertex = UnityObjectToClipPos(v.vertex);
+				output.uv = v.uv;
+
+				return output;
+			}
+
+			// _MainTexをsampler2DArrayとして宣言
+			UNITY_DECLARE_TEX2DARRAY(_MainTex);
+
+			fixed4 frag (v2f input) : SV_Target
+			{
+				// vertから流れてきたinputからunity_StereoEyeIndexを更新
+				UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
+
+				// inputを使用しなくても、以下の関数でunity_StereoEyeIndexを更新可能(今回は不使用)
+				// UNITY_SETUP_INSTANCE_ID(i);
+				
+				// unit_StereoEyeIndex番目のテクスチャをサンプリング
+				return UNITY_SAMPLE_TEX2DARRAY(_MainTex,
+					float3((input.uv).xy, (float)unity_StereoEyeIndex));
+			}
+
+            ENDCG
+        }
+    }
+}
